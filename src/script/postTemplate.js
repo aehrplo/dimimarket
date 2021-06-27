020fb7630dd7dc6ee05d7bd105148f44da2c8ca7
electrochemistry04 <electrochemistry04@outlook.com>
2021-06-28 07:48:42 +0900

    commit

--- src/script/postTemplate.js
+++ src/script/postTemplate.js
@@ -1,34 +1,55 @@
 let buildPost = (img) => {
     let post = `
-    <section>
-        <img class="mdThumb" src="../img/${img}.png">
-        <div class="mdThumbInfo">
-            <span class="leftThumbInfo">
-                <span class="mdUsername">2435 디테계</span>
-                <span class="mdUploadTime">10분전</span>
-            </span>
-            <span class="mdTarget">목표수량 5/10</span>
-        </div>
-        <div class="mdDescription">
-            팔아요
-        </div>
-        <div class="tags">
-            <span>
-                <span class="leftTags">
-                    <img class="HeartICO" src="../img/heartfilled.svg">
-                    <span class="mdHeart">8</span>
+    <a href="../article">
+        <section class="thumbnail">
+            <img class="mdThumb" src="../src/img/${img}.png">
+            <div class="mdThumbInfo">
+                <span class="leftThumbInfo">
+                    <span class="mdUsername">2435 디테계</span>
+                    <span class="mdUploadTime">10분전</span>
                 </span>
+                <span class="mdTarget">목표수량 5/10</span>
+            </div>
+            <div class="mdDescription">
+                팔아요
+            </div>
+            <div class="tags">
                 <span>
-                    <img class="ChatICO" src="../img/chat.svg">
-                    <span class="mdChat">13</span>
+                    <span class="leftTags">
+                        <img class="HeartICO" src="../src/img/heartfilled.svg">
+                        <span class="mdHeart">8</span>
+                    </span>
+                    <span>
+                        <img class="ChatICO" src="../src/img/chat.svg">
+                        <span class="mdChat">13</span>
+                    </span>
                 </span>
-            </span>
-            
-            <span class="mdPrice">13,000원</span>
-        </div>
-    </section>
+                
+                <span class="mdPrice">13,000원</span>
+            </div>
+        </section>
+    </a>
     `
     return post;
+}
+let buildDET = (img) => {
+    
+}
+let det = `
+<section class="detail">
+    <img class="mdIMG" src="../img/${1}.png">
+    <div class="mdInfo">
+        <span class="leftThumbInfo">
+            <span class="mdUsername">2435 디테계</span>
+            <span class="mdUploadTime">10분전</span>
+        </span>
+        <span class="mdTarget">목표수량 5/10</span>
+    </div>
+    <hr>
+</section>`
+
+let paragraph = () => {
+    document.getElementById('paragraph').innerHTML = det;
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
@@ -36,7 +57,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     for(let i = 0; i < 5;  i++){
         let random1 = Math.floor(Math.random()*1000)%6;
         let random2 = Math.floor(Math.random()*1000)%6;
-        asdf += `<div>${buildPost(random1)}${buildPost(random2)}</div><br>`
+        let random3 = Math.floor(Math.random()*1000)%6;
+        asdf += `<div>${buildPost(random1)}${buildPost(random2)}${buildPost(random3)}</div><br>`
     }
     document.getElementById('paragraph').innerHTML = asdf;
 });
